dd82c6f928485c8237331a5bee437f81bd730df7
mjayfinley <mjayfinley@gmail.com>
2019-10-14 10:31:34 -0500

    completed auth with guard and interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +22,7 @@ import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
 import { AuthService } from './auth/auth.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
     declarations: [
@@ -46,6 +47,7 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
         DataStorageService,
         RecipesResolverService,
         AuthService,
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
     ],
     bootstrap: [AppComponent],
 })
